924f9d8ec75972b285864f1145524224ee3eacfe
marchdown <marchdown@gmail.com>
2023-05-22 23:39:01 +0300

    foo

--- main/dist/game.js
+++ main/dist/game.js
@@ -2,4 +2,5 @@ javascript:(function() {
     var script = document.createElement('script');
     script.src = 'https://raw.githubusercontent.com/uprootiny/GnosticRealms/master/main/dist/game.js';
     document.body.appendChild(script);
+    alert("You're in now");
 })();
